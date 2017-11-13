a1176b1e62d5ff5a9118de732f73209df89da967
Unknown <alequis01@gmail.com>
2017-11-12 23:35:13 -0200

    Add collision, enemy destruction and score

--- Assets/Scripts/PersonMovement.cs
+++ Assets/Scripts/PersonMovement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PersonMovement : MonoBehaviour {
 
+	private Player mPlayer;
 	// Use this for initialization
 	void Start () {
+		mPlayer = FindObjectOfType<Player>();
 		StartCoroutine(Move(.05f));
 	}
 	
 	private IEnumerator Move(float speed){
-		while(transform.position.x > -13){
+		while(transform.position.x > -11){
 			transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+
 			yield return null;
 		}
+		mPlayer.Score += 10;
+		Debug.Log("Pontuação: " + mPlayer.Score);
+		Destroy(gameObject);
 	}
+
+	
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,9 +2,10 @@
 
 public class PlayerController : MonoBehaviour {
 
+	private Player mPlayer;
 	// Use this for initialization
 	void Start () {
-		
+		mPlayer = GetComponent<Player>();
 	}
 	
 	// Update is called once per frame
@@ -15,6 +16,20 @@ public class PlayerController : MonoBehaviour {
 		}else if(Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("s")){
 			if(transform.position.y > -3)
 				transform.position = new Vector3(transform.position.x, transform.position.y - 3, transform.position.z);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "Enemy") {
+			if(mPlayer.Lives > 1) {
+				mPlayer.Lives--;
+				Debug.Log("Vidas restante: " + mPlayer.Lives);
+			} else {
+				Debug.Log("Fim do jogo. Pontuação total: " + mPlayer.Score);
+				FindObjectOfType<PeopleController>().StopAllCoroutines();
+			}
+			Destroy(other.gameObject);
+			//other.GetComponent<PersonMovement>().StopAllCoroutines();
 		}
 	}
 }
